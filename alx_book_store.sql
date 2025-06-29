b0747ccf3c6c4a73ae3a27e5cbcb37947e6bf1b3
Alpatson Cobbina Siaw <cobbinaalpatson@gmail.com>
2025-06-29 15:57:09 +0000

    Created a database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -8,6 +8,9 @@ mydb = mysql.connector.connect(
     database="alx_book_stores"
 )
 
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
